1b5c7ddc528e2463bf68bc5007552c75cb951762
Samuel <nushuhu51@gmail.com>
2026-06-20 22:33:40 +0300

    varibles

---
+++ introduction.py
@@ -0,0 +1,9 @@
+# This is my first python program!
+print("I like pizza.")
+print("It's really good!")
+
+
+
+
+
+
---
+++ var.py
@@ -0,0 +1,27 @@
+# Variable = A container for a value (string, integer, float, boolean)
+# a variable behave as if it was the value it contains
+
+# strings
+first_name = "samuel"
+food = "pizza"
+email = "samuel@example.com"
+
+print(f"hello {first_name}")
+print(f"you like {food}")
+print(f"your email is {email}")
+#
+# Integers
+age = 25
+quantity = 3
+num_of_students = 30
+
+print(f"you are {age} years old")
+print(f"you are buying {quantity} items")
+print(f"your class has {num_of_students} students")
+
+# Floats
+price = 10.99
+gpa = 3.2
+
+print(f"the price is ${price}")
+print(f"your gpa is {gpa}")
