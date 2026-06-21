4bad54daa9f324f4104d67bdc20de14ffe0cf0ea
Samuel <nushuhu51@gmail.com>
2026-06-21 11:04:25 +0300

    operations

---
+++ PYTHON/arthi_operations.py
@@ -0,0 +1,18 @@
+
+
+
+friends = 5
+## adding 1 to friends
+# friends = friends + 1 is the same as friends += 1
+friends += 1
+## subtracting 1 from friends
+friends -= 1
+## multiplying friends by 2
+friends *= 2
+## dividing friends by 2
+friends /= 2
+## raising friends to the power of 2
+friends **= 2
+ ## taking the modulus of friends by 2
+friends %= 2
+print(friends)
