75099902833d6cb4caf4c4f31c48c2a85bcf1740
Samuel <nushuhu51@gmail.com>
2026-06-21 09:53:57 +0300

    area and perimeter of rectangle

--- PYTHON/Exercise_1.py
+++ PYTHON/Exercise_1.py
@@ -1,1 +1,9 @@
 # Rectangle area and preimeter calculation.
+length = float(input("Enter the length of the rectangle: "))
+width = float(input ("Enter the width of the rectangle: "))
+
+area = length*width
+perimeter = 2*(length*width)
+
+print(f"The area of the rectangle is {area}cm^2")
+print(f"The perimeter of the rectangle is {perimeter}cm")
