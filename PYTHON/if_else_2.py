ce055fc36e8732d526d56f4629b1e6b46646c879
Samuel <nushuhu51@gmail.com>
2026-06-21 15:06:58 +0300

    if else 2

---
+++ PYTHON/if_else_2.py
@@ -0,0 +1,26 @@
+"""
+response = input("Would you like food? (Y/N): ")
+
+if response == "Y":
+    print("Have some food!")
+else:
+    print("No food for you
+    !") 
+"""
+# and here is the second example of if else statement
+"""
+name = input("Enter your name: ")
+
+if name == "":
+    print("You did not type in your name!")
+else:
+    print(f"Hello {name}!")
+    
+"""
+# and here is the third example of if else statement
+for_sale = False
+
+if for_sale:
+    print("This is for sale!")
+else:
+    print("This item is not for sale!")
