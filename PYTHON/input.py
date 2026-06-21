246ca6a18a7ea9877f159929adbc9cd6847df25a
Samuel <nushuhu51@gmail.com>
2026-06-21 09:38:15 +0300

    input_data

---
+++ PYTHON/input.py
@@ -0,0 +1,11 @@
+# input is a built in function in python that allows the user to take input from the console.and returns the input data as a string.
+
+name = input("what is your name ?: ")
+age = input("How old are you ?: ")
+
+age = int(age)
+age = age + 1
+
+print(f"Hello {name}, welcome to python programing language.")
+print("HAPPY BIRTHDAYA!")
+print(f"You are {age} years old.")
