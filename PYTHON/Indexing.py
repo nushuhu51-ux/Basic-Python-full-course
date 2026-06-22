85fa518371354e39a4b980bc48348e60e67d4a0a
Samuel <nushuhu51@gmail.com>
2026-06-22 10:27:59 +0300

    indexing

---
+++ PYTHON/Indexing.py
@@ -0,0 +1,14 @@
+# Indexing = accessing elements of a sequence using [] (indexing operator)
+#      [start: end: step]
+
+credit_number = "1234-5678-9012-3456"
+
+#print(credit_number[0])
+#print(credit_number[:4])
+#print(credit_number[5:9])
+#print(credit_number[5:])
+#print(credit_number[-1])
+#print(credit_number[::3])
+
+last_digits = credit_number[::-1]
+print(f"xxxx-xxxx-xxxx-{last_digits}")
