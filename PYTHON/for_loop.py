74ca73c1efad5e9d99bdb860900b0ca7ec772dec
Samuel <nushuhu51@gmail.com>
2026-06-24 18:24:48 +0300

    for loops

---
+++ PYTHON/for_loop.py
@@ -0,0 +1,12 @@
+# for loops = excute a block of code a fixed number of times.
+#             you can iterate over a range, string, sequence, tec.
+
+# example- 1
+
+for x in range(1, 21):
+    if x == 13:
+        break
+    else:
+        print(x)
+    
+print("HAPPY NEW YEAR!!")
